0068c4f1952212829db7690d569ff8d0ec135db7
Keyance <klara.konopacova@seznam.cz>
2023-12-02 23:28:17 +0100

    Přidejte soubory projektu.

---
+++ L8UkolFarma/Slepice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L8UkolFarma
+{
+    public class Slepice : Zvire, ISnasiVejce
+    {
+    }
+}
---
+++ L8UkolFarma/Zvire.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L8UkolFarma
+{
+    public abstract class Zvire
+    {
+        public string Jmeno;
+        public string OzviSe(string Jmeno)
+        {
+            return "Ahoj, já jsem " + Jmeno;
+        }
+    }
+}
